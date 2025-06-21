556969d2d84922f6a211186b4ac3344bd71b7fdb
amrkhalid <amr_khalid_2013@hotmail.com>
2025-06-21 06:23:28 +0300

    last changes updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from '@/store';
-import { AppRoutes } from '@/routes';
-import ErrorBoundary from '@/components/error-boundary';
-import { Toaster } from './components/ui/toaster';
+import React, { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "@/store";
+import { AppRoutes } from "@/routes";
+import ErrorBoundary from "@/components/error-boundary";
+import { Toaster } from "./components/ui/toaster";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    const savedLang = localStorage.getItem("appLang");
+    if (savedLang && i18n.language !== savedLang) {
+      i18n.changeLanguage(savedLang);
+    }
+  }, []);
+
   return (
     <ErrorBoundary>
       <Provider store={store}>
